16930c5786e0ac1f3c9aad4eff8840a62e04ff48
Tristan Hendricks <moonknight1355@gmail.com>
2024-09-14 19:23:27 +0200

    Initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "../styles/Projects.css";
+import FolderOpenRoundedIcon from "@material-ui/icons/FolderOpenRounded";
+import FadeInSection from "./FadeInSection";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Carousel from "react-bootstrap/Carousel";
+import ExternalLinks from "./ExternalLinks";
+
+class Projects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    const spotlightProjects = {
+      "Name of Project": {
+        //title: "name",
+        //desc:
+          //"Example",
+        //techStack: "C# (UNITY)",
+        //link: "https://github.com/",
+        //open: "https://.itch.io/",
+        //image: ""
+      },
+    };
+    const projects = {
+      "Project name": {
+        desc:
+          "Example",
+        techStack: "",
+        link: "https://github.com/",
+        open: "https://www.heroku.com/home"
+      },
+    };
+
+    return (
+      <div id="projects">
+        <div className="section-header ">
+          <span className="section-title">/ projects</span>
+        </div>
+        <Carousel>
+          {Object.keys(spotlightProjects).map((key, i) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={spotlightProjects[key]["image"]}
+                alt={key}
+              />
+              <div className="caption-bg">
+                <Carousel.Caption>
+                  <h3>{spotlightProjects[key]["title"]}</h3>
+                  <p>
+                    {spotlightProjects[key]["desc"]}
+                    <p className="techStack">
+                      {spotlightProjects[key]["techStack"]}
+                    </p>
+                  </p>
+                  <ExternalLinks
+                    githubLink={spotlightProjects[key]["link"]}
+                    openLink={spotlightProjects[key]["open"]}
+                  ></ExternalLinks>
+                </Carousel.Caption>
+              </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+        <div className="project-container">
+          <ul className="projects-grid">
+            {Object.keys(projects).map((key, i) => (
+              <FadeInSection delay={`${i + 1}00ms`}>
+                <li className="projects-card">
+                  <div className="card-header">
+                    <div className="folder-icon">
+                      <FolderOpenRoundedIcon
+                        style={{ fontSize: 35 }}
+                      ></FolderOpenRoundedIcon>
+                    </div>
+                    <ExternalLinks
+                      githubLink={projects[key]["link"]}
+                      openLink={projects[key]["open"]}
+                    ></ExternalLinks>
+                  </div>
+
+                  <div className="card-title">{key}</div>
+                  <div className="card-desc">{projects[key]["desc"]}</div>
+                  <div className="card-tech">{projects[key]["techStack"]}</div>
+                </li>
+              </FadeInSection>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
